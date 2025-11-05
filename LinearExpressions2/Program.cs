675f1f1e405b9882ea75b4d0bf22d634f98a8a22
ArturBorovyi <ipz253_bao@student.ztu.edu.ua>
2025-11-05 17:43:01 +0200

    Added LinearExpressions2

---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,22 @@
+﻿double m, n;
+Console.Write("Введiть додатнє m: ");
+m = double.Parse(Console.ReadLine());
+Console.Write("Введiть невiд'ємне n: ");
+n = double.Parse(Console.ReadLine());
+
+double z1, z2;
+z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m*m*m*n) + n*m + m*m - m);
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+Console.WriteLine($"z1 = {z1}, z2 = {z2}");
+
+double a, b, x;
+Console.Write("Введiть ненульове a: ");
+a = double.Parse(Console.ReadLine());
+Console.Write("Введiть b: ");
+b = double.Parse(Console.ReadLine());
+Console.Write("Введiть x: ");
+x = double.Parse(Console.ReadLine());
+
+double y;
+y = 2.4 * Math.Abs((x * x + b) / a) + (a - b) * Math.Sin(a - b) * Math.Sin(a - b) + 0.01 * (x - b);
+Console.WriteLine($"y = {y}");
