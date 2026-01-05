f4e5de6307e38bf0278590278a5c8046b5385eb9
ArturBorovyi <ipz253_bao@student.ztu.edu.ua>
2026-01-05 18:14:13 +0200

    Виправив помилку в 1, додав gitignore

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -11,7 +11,7 @@ d = double.Parse(Console.ReadLine());
 double x, y, z, r;
 x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (a * a) / (b * b);
 y = (5 * (a + b) * (c - d)) / (0.5 * c) + d * d * (a * a - b * b) / (b - a);
-z = (Math.Pow(x * x - 2 * x, 3) - 4 * (x * x * x * x + 1) * (1 - b)) / (5 * a + 3 * b);
+z = (Math.Pow(x * x - 2 * x, 3) - 4 * (x * x * x * x + 1)) * (1 - b) / (5 * a + 3 * b);
 r = (0.5 * a + 0.75 * b - 1.4) / (3 * c + 1) + 1 / (a - c);
 
 Console.WriteLine($"x = {x}, y = {y}, z = {z}, r = {r}");
